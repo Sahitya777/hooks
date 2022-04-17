1f90361206711020df96d0d5e73a7aa71e991f14
Sahitya77 <sahityanijhawan@gmail.com>
2022-04-17 22:46:30 +0530

    UseEffect hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import StateTutorial from './UseState/StateTutorial';
 import ReducerTutorial from './UseReducer/ReducerTutorial';
+import EffectTutorial from './UseEffect/EffectTutorial';
 import './App.css';
 
 function App() {
   
   return (
     <div className="App">
-      <ReducerTutorial/>
+      <EffectTutorial/>
     </div>
   );
 }
